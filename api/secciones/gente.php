55198c4c47cb39fae35f752d61e89534f82a3eaa
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2025-02-18 12:42:54 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "AUMENTO MASIVO DE POBLACION EN ZONAS RURALES DE PERU",
+"autor" => "LATINA",
+"resumen" => "se realizo un estudio donde se encontro que hubo un crecieminto en la tasa de natalidad en la parte sur del Peru",
 ];
 ?>
