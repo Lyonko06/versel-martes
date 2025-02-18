53df563ca28b226e2a4c69b530185eede9c792de
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2025-02-18 12:48:41 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "SOCIEDAD PRIVADA DEL NORTE ",
+"autor" => "EL DIARIO EL CONGRESO",
+"resumen" => "SOCIEDAD PERUANA",
 ];
 ?>
