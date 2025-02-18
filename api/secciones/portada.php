e7bb1d4ab7330b82b84c0cbcded671fe3807fbb1
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2025-02-18 12:47:22 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Diario Carlo Leon,\"Pobre, pero Honrado\"",
+"titulo" => "Diario Carlo Leon",
 "autor" => "Carlo",
 "resumen" => "El resumen Febrero 2025",
 ];
