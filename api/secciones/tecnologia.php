0efe0cff100fa9a45be252ca4315ae7519a9cdf9
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2025-02-18 12:49:13 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "SPRING BOOT EL NUEVO ALCNAZE DE LAS TECNOLOGIAS",
+"autor" => "JAVA ENTERPRISE",
+"resumen" => "SE UTILZIA PARA EL BACKEND",
 ];
 ?>
