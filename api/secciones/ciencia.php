c0d47e85ac12ce227125efb5d9ab2153acf4ca1e
Lyonko06 <109053235+Lyonko06@users.noreply.github.com>
2025-02-18 12:37:01 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "PRIMER ESTACION ESPACIAL EN PERU",
+"autor" => "EL COMERCIO",
+"resumen" => "PRIMERA ESTACION ESPACIAL EN LATAM",
 ];
 ?>
